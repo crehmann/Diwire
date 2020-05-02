c0f190a0c4d69e9b772cf69eaaf87af8f540e99a
crehmann <1273098+crehmann@users.noreply.github.com>
2020-05-02 06:47:37 +0200

    only use public or internal constructors.

--- src/Diwire.Analyzers/Diwire.Analyzers/Helpers/RegistrationInfo.cs
+++ src/Diwire.Analyzers/Diwire.Analyzers/Helpers/RegistrationInfo.cs
@@ -12,7 +12,9 @@ namespace Diwire.Analyzers.Helpers
                 : 0;
             var lifetimeIndex = constructorIndex + 1;
             FromType = (INamedTypeSymbol)registerTypeAttribute.ConstructorArguments[0].Value;
-            Constructor = ((INamedTypeSymbol)registerTypeAttribute.ConstructorArguments[constructorIndex].Value).Constructors.Single();
+            Constructor = ((INamedTypeSymbol)registerTypeAttribute.ConstructorArguments[constructorIndex].Value).Constructors
+                .Where(x => x.DeclaredAccessibility == Accessibility.Public || x.DeclaredAccessibility == Accessibility.Internal)
+                .Single();
             Lifetime = ((int)registerTypeAttribute.ConstructorArguments[lifetimeIndex].Value);
         }
 
